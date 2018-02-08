2c7bd4d534e5a43f09784dad787f3afe207535e3
Jose Gómez <josegomezgadea@gmail.com>
2018-02-08 17:35:39 +0100

    Otra prueba lok

--- PythonServers/Solucion (por terminar).py
+++ PythonServers/Solucion (por terminar).py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import operator
 
-class GetValues: prueba lok
+class GetValues: prueba lo
     def read():
         # Almacenamos el fichero en "file"
         file = open("dc.in", "r")
