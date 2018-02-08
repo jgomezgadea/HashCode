9e06827942851f6e35badba9064e3fce438b03aa
Jose Gómez <josegomezgadea@gmail.com>
2018-02-08 18:10:57 +0100

    Avanzando en GetData.py

---
+++ PythonPizza/GetData.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import numpy as np
+
+class GetData:
+    def read():
+        # Variables
+        pizza = []
+        minIng # Mínimo de elementos de cada tipo
+        maxPorc # Número máximo de elementos por porción
+        # Almacenamos el fichero en "file"
+        file = open("small.in", "r")
+        # Lectura de la primera línea con todos los datos
+        param = file.readline()
+        param = np.fromstring(param, dtype=int, sep=' ')
+
+        matrix = ''
+        for x in range(param[0]):
+            
