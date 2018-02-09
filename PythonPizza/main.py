58e4dc8357daf960a491cded71deaa17220aee4e
Dani Turner <untalturner@gmail.com>
2018-02-09 10:13:37 +0100

    Main inicial

---
+++ PythonPizza/main.py
@@ -0,0 +1,21 @@
+#Variables globales
+    pizza = []          # Matriz de la pizza
+    minIng = None       # Numero minimo de piezas por ingrdiente
+    maxPor = None       # Numero maximo de piezas por porcion
+    listForm = {}       # Lista ordenada de posibles formas de porciones
+    listPor = {}        # Lista de porciones resultado
+
+#TODO: Leer fichero y devolver matriz pizza, minIng y maxPor    (JOSE)
+
+
+#TODO: Repartir la pizza en porciones grandes
+
+
+#TODO: Recolocar piezas sueltas
+
+
+#TODO: Escribir fichero solucion    (JOSE)
+# listPor = { {xi, yi, xf, yf, nt, nm} {...} {...} }
+# xi = fila inicio      # yi = columna inicio
+# xf = fila fin         # yf = columna final
+# nt = numero tomates   # nm = numero champiñones
